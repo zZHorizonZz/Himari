b9afcbce3e85c8163eb025ce5078349ee45532bc
Horizon <danfiala23@gmail.com>
2022-01-27 00:06:20 +0100

    Added classes for mapping and reponse

---
+++ WebServer/src/main/java/com/himari/response/Response.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c)  2022 Daniel Fiala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.himari.response;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class Response<T> implements Future<T> {
+
+    private T response;
+
+    public Response(T response) {
+        this.response = response;
+    }
+
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        return false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    @Override
+    public boolean isDone() {
+        return false;
+    }
+
+    @Override
+    public T get() throws InterruptedException, ExecutionException {
+        return null;
+    }
+
+    @Override
+    public T get(long timeout, @NotNull TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+}
